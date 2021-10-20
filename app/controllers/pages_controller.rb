28e63e736cfca87fc74d2fd4a16a60a8c30d703c
Alexandra Boedtker <alexboedtker@gmail.com>
2021-10-20 17:13:26 +0200

    cleaned

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
   def home
     response = HTTParty.get('https://api.punkapi.com/v2/beers')
-    #take params and search through response[index]["name"] or response[index]["description"]
-    #@beers_names = response.map { |hash| hash["name"] }
-    #@beers_description = response.map { |hash| hash["description"] }
     @search = params["search"]
     @results = []
     if @search.present?
