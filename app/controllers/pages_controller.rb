5d14e9fd6b39779117a0505cc44c73721b6bf245
Alexandra Boedtker <alexboedtker@gmail.com>
2021-10-20 16:23:53 +0200

    beer search works and results are presented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,12 @@ class PagesController < ApplicationController
   def home
     response = HTTParty.get('https://api.punkapi.com/v2/beers')
     #take params and search through response[index]["name"] or response[index]["description"]
-    #create a list with the returned response
-    #what gets returned? what gets displayed?
     #@beers_names = response.map { |hash| hash["name"] }
     #@beers_description = response.map { |hash| hash["description"] }
     @search = params["search"]
+    @results = []
     if @search.present?
-      # @result = @beers_names.find { |name| name == @search["beer_query"] }
-      @results = response.select { |hash| hash["description"].include? @search["beer_query"] }
+      @results = response.select { |hash| (hash["description"].include?(@search["beer_query"]) || hash["name"].include?(@search["beer_query"])) }
     end
-
-    #methods give more results
   end
 end
