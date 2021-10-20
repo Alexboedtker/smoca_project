e2a8ec3954c9d86816c03d6866150447f6b58f61
Alexandra Boedtker <alexboedtker@gmail.com>
2021-10-20 15:51:42 +0200

    results of all beers with floral in name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,14 @@ class PagesController < ApplicationController
     #take params and search through response[index]["name"] or response[index]["description"]
     #create a list with the returned response
     #what gets returned? what gets displayed?
-    @beers_names = response.map { |hash| hash["name"] }
-    @beers_description = response.map { |hash| hash["description"] }
+    #@beers_names = response.map { |hash| hash["name"] }
+    #@beers_description = response.map { |hash| hash["description"] }
     @search = params["search"]
     if @search.present?
-      @result = @beers_names.find { |name| name == @search["beer_query"] }
+      # @result = @beers_names.find { |name| name == @search["beer_query"] }
+      @results = response.select { |hash| hash["description"].include? @search["beer_query"] }
     end
 
-    #need to use @search["beer_query"] to search through the response for the correct beer
-    #
+    #methods give more results
   end
 end
