3064b1c89ef91a9e595ece256bc6fe4ed6913e4a
Alexandra Boedtker <alexboedtker@gmail.com>
2021-10-20 15:30:37 +0200

    first prototype complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
   def home
     response = HTTParty.get('https://api.punkapi.com/v2/beers')
+    #take params and search through response[index]["name"] or response[index]["description"]
+    #create a list with the returned response
+    #what gets returned? what gets displayed?
+    @beers_names = response.map { |hash| hash["name"] }
+    @beers_description = response.map { |hash| hash["description"] }
+    @search = params["search"]
+    if @search.present?
+      @result = @beers_names.find { |name| name == @search["beer_query"] }
+    end
+
+    #need to use @search["beer_query"] to search through the response for the correct beer
+    #
   end
 end
